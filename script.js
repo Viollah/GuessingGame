f2a2a4f9067011f4c2757926c9fc855522543709
Viollah <viollah0119@gmai.com>
2022-02-22 17:46:20 +0200

    I included 1 and 100 as part of the numbers on js

--- script.js
+++ script.js
@@ -3,18 +3,22 @@
     var againButton = document.getElementById('againButton');
     var output = document.getElementById('outputText');
  
-    var randomNumber = Math.ceil(Math.random()*100);
+    var randomNumber = Math.ceil(Math.random()*101);
  
     function checkNumber(){
       var input = document.getElementById('userInput').value;
       if(input == randomNumber){
         output.innerHTML="Your guess is right "+ ", "+ " it was "+ randomNumber;
         output.style.color="green";
+
+        
+       
+
       }
-      else if(input>randomNumber && input<100){
+      else if(input>randomNumber && input<101){
         output.innerHTML="Your guess is too high";
       }
-      else if(input<randomNumber && input>1){
+      else if(input<randomNumber && input>0){
         output.innerHTML="Your guess is too low";
       }
       else if(input<1){
@@ -24,8 +28,12 @@
         output.innerHTML="That's not a number!";
       }
       else{
-        output.innerHTML="Higher, it has to be between 1 and 100";
+        output.innerHTML="oops, it has to be between 1 and 100";
       }
+
+      setTimeout(function(){
+        output.innerHTML = "";
+      }, 2000);
  
     }
  
